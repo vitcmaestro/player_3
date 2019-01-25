c898eb51de109919b235000cc96d031cad3a3ff6
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-25 16:56:00 +0530

    Update and rename continue.py to mergeandsort.py

---
+++ mergeandsort.py
@@ -0,0 +1,32 @@
+m,n = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+i =0
+j =0
+le = min(m,n)
+ans =[]
+while(i<le and j<le):
+    print(i,j)
+    if(a[i] == b[j]):
+        ans.append(a[i])
+        ans.append(b[j])
+        i+=1
+        j+=1
+    elif(a[i]<b[j]):
+        ans.append(a[i])
+        i+=1
+    elif(a[i]>b[j]):
+        ans.append(b[j])
+        j+=1
+if(m>n):
+    for x in range(i,m):
+        ans.append(a[x])
+elif(m<n):
+    for x in range(j,n):
+        ans.append(b[x])
+for i in range(len(ans)):
+    if(i == len(ans)-1):
+        print(ans[i],end ="")
+    else:
+        print(ans[i],end =" ")
+    
