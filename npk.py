028897f5f4ffb86ac4ab4735ad9c59506134d291
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-26 10:06:15 +0530

    Create npk.py

---
+++ npk.py
@@ -0,0 +1,10 @@
+def fact(n):
+    if(n == 0):
+        return 1
+    x = 1
+    for i in range(2,n+1):
+        x = x *i
+    return x
+n,k = map(int,input().split())
+ans = (fact(n)//fact(n-k))
+print(ans)
